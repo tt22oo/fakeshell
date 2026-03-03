1221c874a559a4fa5a8b514d1dd71d2a230c8b9d
tt22oo <rntk25w7rq@privaterelay.appleid.com>
2026-03-03 16:16:41 +0900

    Update types.go

--- shell/dir/types.go
+++ shell/dir/types.go
@@ -3,18 +3,21 @@ package dir
 import "sync"
 
 type Entry struct {
+	Mu       sync.Mutex
 	Name     string            `json:"name"`
 	Type     string            `json:"type"`
 	Data     string            `json:"data"`
 	Children map[string]*Entry `json:"children"`
 }
 
-type Dir struct {
-	Mu      sync.Mutex
-	Entries map[string]*Entry
+type Directory struct {
+	Mu       sync.Mutex
+	HomePath string
+	Entry    *Entry
+	Entries  map[string]*Entry
 }
 
 const (
-	File      string = "file"
-	Directory string = "directory"
+	TypeFile      string = "file"
+	TypeDirectory string = "directory"
 )
